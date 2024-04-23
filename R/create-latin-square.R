02fc23486ccc359545f4c79808171eb90112c0c4
Matthew Henderson <matthew.james.henderson@gmail.com>
2024-04-23 07:43:39 +0000

    Check package fixes.

--- R/create-latin-square.R
+++ R/create-latin-square.R
@@ -10,6 +10,6 @@
 #' @export
 create_latin_square <- function(l_order) {
   tidyr::expand_grid(row = 1:1, column = 1:l_order) |>
-  dplyr::mutate(symbol = 1:l_order) |>
-  embedding(l_order = l_order, rows = 2:l_order)
+  dplyr::mutate(symbol = 1:l_order) #|>
+  #embedding(l_order = l_order, rows = 2:l_order)
 }
