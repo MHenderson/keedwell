9c041e8713a96e9c56fa5306aa1e3af7f73f47a0
Matthew Henderson <matthew.james.henderson@gmail.com>
2024-05-22 13:48:16 +0000

    Import functions from ryser-in-r.

--- R/edge-tbl.R
+++ R/edge-tbl.R
@@ -31,3 +31,29 @@ edge_tbl <- function(R, i, l_order = 3) {
   
   return(edge_df)
 }
+
+#' Symbols missing from rows data frame
+#'
+#' @param R latin rectangle
+#' @param i row index
+#' @param l_order size of latin square R is going to be embedded into
+#'
+#' @return The edge data frame.
+edge_tbl_2 <- function(R, i, l_order = 3) {
+  
+  all_symbols <- 1:l_order
+  
+  # symbols used in row i
+  used <- R |> dplyr::filter(row == i) |> dplyr::pull(symbol)
+  
+  # symbols missing from row i
+  missing <- setdiff(all_symbols, used)
+  
+  # edge data frame for column i
+  edge_df <- tibble::tibble(
+    to = paste0("s", missing)
+  ) |>
+    dplyr::mutate(from = paste0("r", i))
+  
+  return(edge_df)
+}
--- R/next-row.R
+++ R/next-row.R
@@ -37,3 +37,73 @@ next_row <- function(R, l_order) {
   EE <- igraph::ends(mg, igraph::E(mg))
   return(as.numeric(gsub("s", "", EE[,2])))
 }
+
+#' Find a random new row for a latin rectangle
+#'
+#' @param R A latin rectangle
+#' @param i Number of columns to add
+#' @param l_order Order of R
+#'
+#' @return A latin square with more rows.
+#' @export
+next_row_random <- function(R, i, l_order) {
+  
+  R |>
+    dplyr::bind_rows(
+      tibble::tibble(
+        row = rep(i, l_order),
+        column = 1:l_order,
+        symbol = sample(1:l_order, l_order)
+      )
+    )
+  
+}
+
+#' Find a compatible row for extending a latin rectangle
+#'
+#' Given an input latin rectangle this function will
+#' generate a new row that can be added to the latin
+#' rectangle.
+#'
+#' The method used is to create a bipartite graph
+#' with vertex partitions for columns and symbols
+#' missing from columns and then find a maximum
+#' matching in that bipartite graph.
+#'
+#' @param R A latin rectangle
+#' @param i Number of columns to add
+#' @param l_order Order of R
+#'
+#' @return A latin rectangle with more rows.
+#' @export
+next_row_matching <- function(R, i, l_order) {
+  bg <- to_tidygraph(R, l_order)
+  
+  m <- igraph::max_bipartite_match(bg)
+  
+  # names of edges in the matching
+  matching_names <- match(m$matching, names(m$matching))
+  
+  # add a matching indicator to the edges
+  bg <- bg |>
+    tidygraph::activate(edges) |>
+    dplyr::mutate(
+      matching = to == matching_names[from]
+    )
+  
+  # just the matching itself, as a graph
+  mg <- bg |>
+    tidygraph::activate(edges) |>
+    dplyr::filter(matching)
+  
+  EE <- igraph::ends(mg, igraph::E(mg))
+  
+  R |>
+    dplyr::bind_rows(
+      tibble::tibble(
+        row = rep(i, l_order),
+        column = 1:l_order,
+        symbol = as.numeric(gsub("s", "", EE[,2]))
+      )
+    )
+}
